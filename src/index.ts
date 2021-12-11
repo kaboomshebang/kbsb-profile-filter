92641ab9bd7d2b726f9c9d0b8ae5ccf6cb793d13
Fred <fred@fastmail.nl>
2021-12-11 06:13:31 +0100

    Setup logic to fetch and process data.json

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log('Test');
+const url: string = 'data.json';
+
+// define a Dev type according to the expected json structure
+type Dev = {
+	userId: number;
+	name: string;
+	skills: string[];
+	color: string;
+	roles: string;
+};
+
+class Developer {
+	person: Dev;
+	visible: boolean;
+
+	constructor(d: Dev) {
+		this.person = d;
+		this.visible = true;
+	}
+}
+
+// create an async function that fetches json
+// fetchData() returns a promise
+async function fetchData(url: string) {
+	const res = await fetch(url);
+	return await res.json();
+}
+
+// call fetchData and handle the promise
+// chain two .then() methods to get the json data
+fetchData(url)
+	.then((e) => e)
+	.then((e) => {
+		//! do you have to call a function to get the data out of the promise?
+		createDev(e.developers);
+	});
+
+let devs: Developer[] = [];
+
+function createDev(data: Dev[]) {
+	data.forEach((e) => {
+		devs.push(new Developer(e));
+	});
+}
+
+console.log('Devs: ', devs);
